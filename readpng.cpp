45b872e1140adf23d43c132ebeebb334b716213a
magoeke <magoeke@htwg-konstanz.de>
2016-11-18 14:43:28 +0100

    Transforms a image to a grayscale image.

--- readpng.cpp
+++ readpng.cpp
@@ -5,7 +5,22 @@
 int main() {
 	std::cout << "Start to read png" << std::endl;
 
+	Magick::Image image;
 
-	std::cout << "It looks good." << std::endl;
+	try {
+		image.read("image.png");
+
+		std::cout << "File name: " << image.fileName() << std::endl;
+		Magick::Geometry size = image.size();
+		std::cout << "Width: " << size.width() << std::endl;
+		std::cout << "Height: " << size.height() << std::endl;
+
+		image.type( Magick::GrayscaleType );
+		image.write("image2.png");
+
+	} catch(Magick::Exception &error_) {
+		std::cout << "An error encountered. " << error_.what() << std::endl;
+	}
+
 	return 0;
 }
