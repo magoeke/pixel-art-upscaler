2b0fa3a52573703f6cf25cd98618744cbe342f04
magoeke <magoeke@htwg-konstanz.de>
2016-11-18 21:27:57 +0100

    Almost implemented nearest neighbor for cpu.

--- readpng.cpp
+++ readpng.cpp
@@ -2,51 +2,52 @@
 
 #include <Magick++.h>
 
+#include <math.h>
+
+#define FACTOR 4
+
 int main() {
 	std::cout << "Start to read png" << std::endl;
 
-	Magick::Image image;
+	Magick::Image old;
 
 	try {
-		image.read("image.png");
+		old.read("image.png");
 
-		std::cout << "File name: " << image.fileName() << std::endl;
-		Magick::Geometry size = image.size();
-		std::cout << "Width: " << size.width() << std::endl;
-		std::cout << "Height: " << size.height() << std::endl;
+		//std::cout << "File name: " << image.fileName() << std::endl;
+		Magick::Geometry old_geo = old.size();
+		//std::cout << "Width: " << size.width() << std::endl;
+		//std::cout << "Height: " << size.height() << std::endl;
 
-		//image.type( Magick::GrayscaleType );
+		old.type( Magick::GrayscaleType );
 		
+		Magick::Geometry new_geo(old_geo.width() * FACTOR, old_geo.height() * FACTOR);
+		Magick::Image result(new_geo, Magick::Color("black"));
+		//std::cout << "new " << result.size().width() << std::endl;
 
 		// modify pixels
-		image.modifyImage();
+		result.modifyImage();
 
-		Magick::Pixels view(image);
+		Magick::Quantum *old_pixels = old.getPixels(0, 0, old_geo.width(), old_geo.height());
+		Magick::Quantum *new_pixels = result.getPixels(0, 0, new_geo.width(), new_geo.height());
 
-		Magick::Color green("green");
+		//Magick::Quantum *old_pixels = old.getPixels(0, 0, old_geo.height(), old_geo.width());
+                //Magick::Quantum *new_pixels = result.getPixels(0, 0, new_geo.height(), new_geo.width());
 
-		Magick::Quantum *pixels = view.get(0, 0, view.columns(), view.rows());
-		std::cout << "Number of channels: " << image.channels() << std::endl;
-		for(ssize_t x = 0; x < size.height() ; ++x) {
-			//Magick::Quantum quantum = pixels[i];
-			//pixels[i] = 255.0;
-			for(ssize_t y = 0; y < size.width(); ++y) {
-				Magick::Color pixelcolor = image.pixelColor(x, y);
-
-std::cout << pixelcolor.quantumRed() << " of " << QuantumRange << std::endl;
-std::cout << pixelcolor.quantumGreen() << " of " << QuantumRange << std::endl;
-std::cout << pixelcolor.quantumBlue() << " of " << QuantumRange << std::endl;
-				//std::cout << "Pixel color: " <<  << std::endl;
+		int idx = 0;
+		int idy = 0;
+		for(ssize_t y = 0; y < new_geo.height(); ++y) {
+			idy = y / FACTOR;
+			for(ssize_t x = 0; x < new_geo.width(); ++x) {
+				idx = x / FACTOR;
+				new_pixels[( y * new_geo.width()) + x] = old_pixels[(idy * old_geo.width())+idx];
+				std::cout << ((y * new_geo.width()) + x) << " " << ((idy * old_geo.width())+idx) << std::endl;
 			}
-			//std::cout << " " << i << std::endl;
-			//std::cout << " " << quantum.red << std::endl;
-			//*(pixels+i) = green;
 		}
 
+		result.syncPixels();
 
-		view.sync();
-
-		image.write("image2.png");
+		result.write("image2.png");
 
 	} catch(Magick::Exception &error_) {
 		std::cout << "An error encountered. " << error_.what() << std::endl;
