93529331ee7e158626e2ac37356aaff906d45397
magoeke <magoeke@htwg-konstanz.de>
2016-12-20 21:40:40 +0100

    Nearest Neighbor also works now with devIL.

--- C++/nearest_neighbour/nearest_neighbor.cpp
+++ C++/nearest_neighbour/nearest_neighbor.cpp
@@ -1,22 +1,94 @@
 #include <iostream>
-#include <math.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <IL/il.h>
+#include <stdint.h>
 
 #define FACTOR 4
 
 int main() {
-	std::cout << "Start to read png" << std::endl;
- 		
-/*		for(ssize_t y = 0; y < result->rows(); ++y) {
-			idy = y / FACTOR;
-			for(ssize_t x = 0; x < result->columns(); ++x) {				
-				idx = x / FACTOR;
-				for(ssize_t z = 0; z < number_of_channels; ++z) {
-					Magick::Quantum old = old_pixels[(idy * old_geo.width() * number_of_channels)+ (idx * number_of_channels) + z];
-					new_pixels[( y * new_geo.width() * number_of_channels) + (x * number_of_channels) + z] = old;
-				}
-			}
-		}*/
-	std::cout << "Finished to process image" << std::endl;
+    std::cout << "Start to read png" << std::endl;
+    
+    ILuint handle, width, height;
 
+    const char *szFilenameIn = "image.png";
+    const char *szFilenameOut = "image2.png";
+	
+    ilInit();
+    ilEnable(IL_ORIGIN_SET);
+    ilGenImages(1, &handle);
+    ilBindImage(handle);
+
+    // Load image
+    ILboolean loaded = ilLoadImage(szFilenameIn);
+    if (loaded == IL_FALSE) {
+        fprintf(stderr, "ERROR: can't load '%s'\n", szFilenameIn);
+        return 1;
+    }
+    width = ilGetInteger(IL_IMAGE_WIDTH);
+    height = ilGetInteger(IL_IMAGE_HEIGHT);
+
+    // Allocate memory for image data
+    size_t srcSize = width * height * sizeof(uint32_t);
+    uint8_t *srcData = (uint8_t *) malloc(srcSize);
+    size_t destSize = width * FACTOR * height * FACTOR * sizeof(uint32_t);
+    uint8_t *destData = (uint8_t *) malloc(destSize);
+
+    // Init srcData from loaded image
+    // We want the pixels in BGRA format so that when converting to uint32_t
+    // we get a RGB value due to little-endianness.
+    ilCopyPixels(0, 0, 0, width, height, 1, IL_BGRA, IL_UNSIGNED_BYTE, srcData);
+
+    uint32_t *sp = (uint32_t *) srcData;
+    uint32_t *dp = (uint32_t *) destData;
+
+    // If big endian we have to swap the byte order to get RGB values
+    #ifdef WORDS_BIGENDIAN
+    uint32_t *spTemp = sp;
+    for (i = 0; i < srcSize >> 2; i++) {
+        spTemp[i] = swapByteOrder(spTemp[i]);
+    }
+    #endif
+
+
+    // -------------------------------------------------------------- nearest neighbor
+    for(int row = 0; row < height * FACTOR; ++row) {
+        for(int col = 0; col < width * FACTOR; ++col) {
+            dp[(row * width * FACTOR + col)] = sp[((row / FACTOR) * width + (col / FACTOR))];
+        }
+    }
+    // --------------------------------------------------------------
+
+
+    // If big endian we have to swap byte order of destData to get BGRA format
+    #ifdef WORDS_BIGENDIAN
+    uint32_t *dpTemp = dp;
+    for (i = 0; i < destSize >> 2; i++) {
+        dpTemp[i] = swapByteOrder(dpTemp[i]);
+    }
+    #endif
+
+    // Copy destData into image
+    ilTexImage(width * FACTOR, height * FACTOR, 0, 4, IL_BGRA, IL_UNSIGNED_BYTE, destData);
+
+    // Free image data
+    free(srcData);
+    free(destData);
+
+    // Save image
+    ilConvertImage(IL_BGRA, IL_UNSIGNED_BYTE); // No alpha channel
+    ilHint(IL_COMPRESSION_HINT, IL_USE_COMPRESSION);
+    ilEnable(IL_FILE_OVERWRITE);
+    ILboolean saved = ilSaveImage(szFilenameOut);
+
+    ilDeleteImages(1, &handle);
+
+    if (saved == IL_FALSE) {
+        fprintf(stderr, "ERROR: can't save '%s'\n", szFilenameOut);
+        return 1;
+    }
+
+	std::cout << "Finished to process image" << std::endl;
 	return 0;
 }
