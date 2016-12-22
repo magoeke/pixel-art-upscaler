894d8a00ad296582e3d3ad3ed4f6152a0f96f620
magoeke <magoeke@htwg-konstanz.de>
2016-12-22 15:05:44 +0100

    Moved image class in own header file.

--- C++/nearest_neighbour/nearest_neighbor.cpp
+++ C++/nearest_neighbour/nearest_neighbor.cpp
@@ -1,65 +1,11 @@
 #include <iostream>
-#include <unistd.h>
-#include <stdint.h>
-#include <stdlib.h>
 #include <IL/il.h>
-#include <stdint.h>
+#include "image.h"
 
 #define FACTOR 4
 
-class Image {
-    private:
-        const char *name;
-        ILuint width;
-        ILuint height;
-        uint32_t *data;
-    public:
-        ILuint getWidth() { return width; }
-        ILuint getHeight() { return height; }
-        uint32_t* getData() { return data; }
-        
-        Image(const char* name) {
-            this->name = name;
-            
-            ILboolean loaded = ilLoadImage(name);
-            if (loaded == IL_FALSE) {
-                fprintf(stderr, "ERROR: can't load '%s'\n", name);
-                //return null;
-            }
-            
-            this->width = ilGetInteger(IL_IMAGE_WIDTH);
-            this->height = ilGetInteger(IL_IMAGE_HEIGHT);
-            
-            uint8_t *srcData = (uint8_t *) malloc(width * height * sizeof(uint32_t));
-            ilCopyPixels(0, 0, 0, width, height, 1, IL_BGRA, IL_UNSIGNED_BYTE, srcData);
-            this->data = (uint32_t *) srcData;
-        };
-        
-        Image(const char* name, ILuint width, ILuint height) {
-            this->name = name;
-            this->width = width;
-            this->height = height;
-            uint8_t *srcData = (uint8_t *) malloc(width * height * sizeof(uint32_t));
-            this->data = (uint32_t *) srcData;
-        };
-        
-        void saveImage() {
-            ilTexImage(width, height, 0, 4, IL_BGRA, IL_UNSIGNED_BYTE, this->data);
-            ilConvertImage(IL_BGRA, IL_UNSIGNED_BYTE); // No alpha channel
-            ilHint(IL_COMPRESSION_HINT, IL_USE_COMPRESSION);
-            ilEnable(IL_FILE_OVERWRITE);
-            ILboolean saved = ilSaveImage(this->name);
-            if (saved == IL_FALSE) {
-                fprintf(stderr, "ERROR: can't save '%s'\n", this->name);
-                //return 1;
-            }
-        }
-        
-        ~Image(){ free(this->data); };
-};
 
 void nearest_neighbor(Image *org, Image *res, int factor) {
-    // -------------------------------------------------------------- nearest neighbor
     uint32_t *dp = res->getData();
     uint32_t *sp = org->getData();
     for(int row = 0; row < res->getHeight(); ++row) {
@@ -67,11 +13,12 @@ void nearest_neighbor(Image *org, Image *res, int factor) {
             dp[(row * res->getWidth() + col)] = sp[((row / factor) * org->getWidth() + (col / factor))];
         }
     }
-    // --------------------------------------------------------------
 };
 
 
 int main() {
+
+    // intialize devIL
     ILuint handle;
     ilInit();
     ilEnable(IL_ORIGIN_SET);
